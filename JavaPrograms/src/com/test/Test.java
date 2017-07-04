164108d58bc003c8eae831a73e6ca118c9ce10f2
MB <MB@192.168.1.7>
2017-07-04 22:49:35 +0530

    adding project

---
+++ JavaPrograms/src/com/test/Test.java
@@ -0,0 +1,15 @@
+package com.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String a = "https://staging.dashboard.com";
+		String b = "/login";
+		String URL = a+b;
+		System.out.println(URL);
+
+	}
+
+}
